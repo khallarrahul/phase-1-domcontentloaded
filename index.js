0f0434b52ea011ed2b046ad383eee60c74e14ebb
RAHUL KHALLAR <khallarrahul@gmail.com>
2023-04-07 14:59:59 -0700

    adds DOMContentLoaded

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    let textUpdate = document.getElementById('text')
+    textUpdate.textContent = "This is really cool!"
+    return textUpdate
+})
+
+
+
